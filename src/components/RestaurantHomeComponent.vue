4271b8195ce357b17f906a863bf8fb7eae7ef229
Federica Pozzer <federica.pozzer@gmail.com>
2023-05-17 18:35:58 +0200

    card button aligned fine

--- src/components/RestaurantHomeComponent.vue
+++ src/components/RestaurantHomeComponent.vue
@@ -11,7 +11,7 @@ export default{
 
 <template>
 
-<div>
+<div class="card-group">
     <!-- Card Ristorante (genrica) -->
     <div class="card">
 
@@ -19,17 +19,17 @@ export default{
         <img :src="restaurantData.image" class="card-img-top img-fluid" alt="restaurantImage">
         
         <!-- card body -->
-        <div class="card-body">
+        <div class="card-body d-flex flex-column">
             <!-- nome ristorante -->
             <h4 class="card-title">{{restaurantData.name}}</h4>
             <!-- tipologie -->
-            <p class="card-text d-flex gap-1">
-                <!-- <span v-for="type in restaurantData.types">
+            <p class="card-text d-flex flex-wrap gap-1 text-secondary">
+                <span v-for="type in restaurantData.types">
                     {{ type.name }}
-                </span> -->
+                </span>
             </p>
 
-            <!-- router link -->
+            <div class="mt-auto">
             <router-link :to="{
                 name : 'restaurant-detail',
                 params: {
@@ -41,6 +41,7 @@ export default{
                     Vedi Menù
                 </button>
             </router-link>
+            </div>
 
         </div>
     </div>
@@ -49,6 +50,7 @@ export default{
 </template>
 
 <style lang="scss" scoped>
+
   
 </style>
 
