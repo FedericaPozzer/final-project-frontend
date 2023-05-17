b696f1cb0768028938ab3b1b26e2ec771803cd32
Alessandro Montenero <alemonte99@icloud.com>
2023-05-17 19:02:01 +0200

    commit

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -10,10 +10,35 @@ export const useCartStore = defineStore("cart", {
   },
   actions: {
     addDish(dish) {
-      this.dishes.push(dish);
+        let isAlreadyInCart = 0;
+        for (let otherDishes of this.dishes)
+        {
+            if(dish.id == otherDishes.id)
+            {
+                isAlreadyInCart = 1;
+            }
+        }
+        if(isAlreadyInCart){
+            for (let otherDishes of this.dishes)
+            {
+                if(dish.id == otherDishes.id)
+                {
+                    otherDishes['quantity'] += 1
+                    console.log(otherDishes)
+                }
+            }
+        }
+        else{
+            dish['quantity'] = 1
+            this.dishes.push(dish);
+        }
+        console.log(isAlreadyInCart)
     },
     deleteDish(key) {
         delete this.dishes[key];
+    },
+    deleteCart(){
+        this.dishes = []
     },
     reset() {
       this.count = 0;
