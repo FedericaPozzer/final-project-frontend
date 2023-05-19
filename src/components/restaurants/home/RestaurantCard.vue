2d1b69ba94dfddbd6fb05e1f1628110ea2abbe3e
Alessandro Montenero <alemonte99@icloud.com>
2023-05-19 11:32:12 +0200

    push

--- src/components/restaurants/home/RestaurantCard.vue
+++ src/components/restaurants/home/RestaurantCard.vue
@@ -18,30 +18,37 @@ export default {
 
 <template>
     <!-- Card -->
-    <div class="card-group">
-        <div class="card">
-            <div class="row">
-                <div class="col-4 image-container">
-                    <img :src="endpoint.endpoint + restaurant.image" class="img-fluid rounded-start" alt="img">
-                </div>
-                
-                <div class="col-8">
-                    <div class="card-body p-0">
-                        <h5 class="card-title mt-md-3">{{ restaurant.name }}</h5>
-                        <div class="d-flex flex-wrap">
-                            <div v-for="RestaurantType in restaurant.types" :key="RestaurantType.id">
-                                <p class="card-text me-2"> {{ RestaurantType.name }}</p>
+    <a :href="'/restaurants/' + restaurant.id">
+        <div class="card-group">
+            <div class="card">
+                <div class="row">
+                    <div class="col-4 image-container">
+                        <img :src="endpoint.endpoint + restaurant.image" class="img-fluid rounded-start" alt="img">
+                    </div>
+                    
+                    <div class="col-8">
+                        <div class="card-body p-0">
+                            <h5 class="card-title mt-md-3">{{ restaurant.name }}</h5>
+                            <div class="d-flex flex-wrap">
+                                <div v-for="RestaurantType in restaurant.types" :key="RestaurantType.id">
+                                    <p class="card-text me-2"> {{ RestaurantType.name }}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
         </div>
-    
-    </div>
-</template> 
-<style>
-.restaurant{
+    </a>
+    </template> 
+    <style scoped>
+    a{
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+    }
+    .restaurant{
     height: 15vh;
     width: 100%;
     border: 2px dotted black;
