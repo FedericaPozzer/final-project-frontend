3e2582147266716873ca274927e80da665981c53
Federica Pozzer <federica.pozzer@gmail.com>
2023-05-19 10:12:23 +0200

    problem solved

--- src/components/restaurants/home/RestaurantsList.vue
+++ src/components/restaurants/home/RestaurantsList.vue
@@ -33,7 +33,7 @@ export default {
         <!-- Row che mostra 1 ristorante a riga o 2 da tablet in su -->
         <div class="row row-cols-1 row-cols-md-2 mt-1 gy-2">
             <!-- Card Ristorante -->
-            <RestaurantCard class="col" v-for="restaurant in restaurants" :restaurant="restaurants"/>
+            <RestaurantCard class="col" v-for="restaurant in restaurants" :restaurant="restaurant"/>
         </div>
     </div>
 </template>
