f62506f33132fd88787afa0dd899a947de814f33
Federica Pozzer <federica.pozzer@gmail.com>
2023-05-19 12:11:00 +0200

    navbar ok

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -10,43 +10,48 @@ export default{
 </script>
 <template>
     <!-- Header -->
-    <header class="d-flex align-items-center">
+    <header class="header-bar p-2">
 
-        <!-- SX -->
-        <div class="logo">
-            <!-- Logo con redirect homepage -->
-            <div>
-                <a href="/">
-                    <img src="../../assets/logo.png" alt="logo" class="img-fluid">
-                </a>
+        <div class="row justify-content-between align-items-center">
+            <!-- SX -->
+            <div class="col-5 col-md-4 col-lg-2 logo">
+                <!-- Logo con redirect homepage -->
+                <div>
+                    <a href="/">
+                        <img src="../../assets/logo.png" alt="logo" class="img-fluid">
+                    </a>
+                </div>
             </div>
-        </div>
         
-        <!-- DX -->
-        <div class="d-flex justify-content-end w-100 gap-2">
-            <!-- Icona ricerca (componente Lens) che al click fa vedere Search -->
-            <div>
-                <Lens />
-            </div>
-            <!-- Redirect al Backend -->
-            <div>
-                Link
+            <!-- DX -->
+            <div class="col-4 col-md-8 d-flex justify-content-end align-items-center">
+                <!-- Icona ricerca (componente Lens) che al click fa vedere Search -->
+                <div>
+                    <Lens />
+                </div>
+                <!-- Redirect al Backend -->
+                <div class="ms-3">
+                    <button class="btn btn-outline-dark" rel="tooltip" title="Area Ristoratori">Area Ristoratori</button>
+                </div>
             </div>
         </div>
 
     </header>
 </template>
 <style lang="scss" scoped>
-header{
+
+
+.header{
     position: fixed;
     z-index: 10;
     height: var(--navbar-height);
-    width: 100%;
-    .logo{
-        width: 20%;
-        height: 80%;
-        margin: 20px;
-    }
+}
+
+.header-bar {
+    // debug
+    // background-color: pink;
+
+    background-color: #ffa500;
 }
 
 </style>
