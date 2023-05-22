3d1cde71cf58e1085d68aab6fcdb4ebebcfdec5a
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-05-22 10:08:34 +0200

    creazione loader

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 
 import './style.css'
 import { createPinia } from 'pinia'
-
+import AppLoader from './components/AppLoader.vue';
 import {router} from './router/index.js'
 
 import App from './App.vue'
@@ -14,6 +14,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 const pinia = createPinia()
 
 createApp(App)
+component('AppLoader',AppLoader)
 .use(pinia)
 .use(router)
 .mount('#app')
