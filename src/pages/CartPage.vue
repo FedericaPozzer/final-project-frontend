791615d6d659c5c3816284b2af63d2950556fca6
Federica Pozzer <federica.pozzer@gmail.com>
2023-05-25 12:25:33 +0200

    carrello opzione vuoto ok!

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -4,9 +4,9 @@
 <script>
 import { useCartStore } from '../stores/cart';
 import { useEndpointStore } from '../stores/endpoint.js';
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
             cart: useCartStore(),
             endpoint: useEndpointStore()
         }
@@ -15,54 +15,54 @@ export default{
 </script>
 
 <template>
-    
     <div class="cart-container">
         <div class="cart-backdrop" @click="$emit('close')"></div>
             <div class="cart" @click="$emit('cartExpanded')">
+                
                 <div class="container">
             
-                    <!-- se il carrello è pieno -->
                     <div v-if="cart.dishes.length" class="">
                         <div class="dishes">
-                        <div class="container">
-            
-                            <div class="dish" v-for="dish, i in cart.dishes">
+                    <div class="container">
+                        <div class="dish" v-for="dish, i in cart.dishes">
     
-                                <img :src=" endpoint.endpoint + dish.image" alt="" srcset="">
+                            <img :src=" endpoint.endpoint + dish.image" alt="" srcset="">
     
-                                <span class="name">
-                                    {{ dish.name }}
-                                </span>
-                            
-                                <div class="quantity-buttons d-flex gap-2 justify-content-center">
-                                    <div @click="dish.quantity != 1 ? dish.quantity-- : dish.quantity = 1" class="d-flex align-items-center justify-content-end">
-                                        -
-                                    </div>
-                                    <div class="d-flex align-items-center justify-content-end">
-                                        {{ dish.quantity }}
-                                    </div>
-                                    <div @click="dish.quantity++" class="d-flex align-items-center justify-content-end">
-                                        +
-                                    </div>
+                            <span class="name">
+                                {{ dish.name }}
+                            </span>
+                            <div class="quantity-buttons d-flex gap-3 justify-content-center fs-4">
+
+                                <div @click="dish.quantity != 1 ? dish.quantity-- : dish.quantity = 1" class="d-flex align-items-center justify-content-end btn-hover">
+                                    -
                                 </div>
-                                
-                                <div @click="cart.removeDish(i)" class="d-flex align-items-center justify-content-end">
+                                <div class="d-flex align-items-center justify-content-end">
+                                    {{ dish.quantity }}
+                                </div>
+                                <div @click="dish.quantity++" class="d-flex align-items-center justify-content-end btn-hover">
+                                    +
+                                </div>
+                            </div>
+                                <div @click="cart.removeDish(i)" class="d-flex align-items-center justify-content-end fs-4">
                                     X
                                 </div>
-                            
                                 <span></span>
                                 <span class="description">{{dish.description}}</span>
-
                                 <div class="d-flex justify-content-end">
                                     {{ (dish.price * dish.quantity).toFixed(2) }}€
                                 </div>
-                            </div>
                         </div>
+                    </div>
                         </div>
-            
-                        <div class="container bottom-cart d-flex w-100 justify-content-center">
+                    </div>
+
+                </div>
+
+                <div v-if="cart.dishes.length" class="container bottom-cart d-flex w-100 justify-content-center">
+
+
                         <div class="button red" @click="cart.deleteCart">
-                        Svuota
+                            Svuota
                         </div>
                         <div class="button" @click="$router.push('/checkout')">
                             <a href="/checkout">
@@ -74,65 +74,74 @@ export default{
                                 </span>
                             </a>
                         </div>
-                        </div>
-                    </div>
 
-                    <!-- se il carrello è vuoto -->
-                    <div v-else>
-                        <h2 class="empty-chart">
-                         Il carrello è vuoto!
-                        </h2>
-                    </div>
-                
-            </div>
+
+                </div>
+        
+
+                <!-- se il carrello è vuoto -->
+                <div v-if="!cart.dishes.length">
+                    <h2 class="empty-chart">
+                        Il carrello è vuoto!
+                    </h2>
+                </div>
+
             </div>
     </div>
 
+
 </template>
 <style lang="scss" scoped>
-a{
+a {
     text-decoration: none;
     color: inherit;
 }
-.cart-container{
+
+.cart-container {
 
     display: flex;
+
     @media (orientation: landscape) {
-          flex-direction: row;
-          .cart-backdrop{
+        flex-direction: row;
+
+        .cart-backdrop {
             height: 100vh;
             width: 60%;
-          }
-          .cart{
+        }
+
+        .cart {
             height: 100vh;
             width: 100%;
-          }
-      }
-      
-      @media (orientation: portrait) {
-          flex-direction: column;
-          .cart-backdrop{
-              height: 20vh;
-              width: 100%;
-            }
-            .cart{
-                width: 100%;
-                height: 80vh;
-            }
-      }
+        }
+    }
+
+    @media (orientation: portrait) {
+        flex-direction: column;
+
+        .cart-backdrop {
+            height: 20vh;
+            width: 100%;
+        }
+
+        .cart {
+            width: 100%;
+            height: 80vh;
+        }
+    }
+
     position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
     top: 0;
     z-index: 10;
-    
-    .cart-backdrop{
+
+    .cart-backdrop {
         background-color: black;
         opacity: var(--backdrop-opacity);
     }
-    
-    .cart{
+
+    .cart {
         background-color: var(--bg-color);
         display: flex;
         flex-direction: column;
@@ -142,20 +151,22 @@ a{
         overflow-y: scroll;
         margin: 0;
     }
-    
-    .dishes{
+
+    .dishes {
         bottom: 5rem;
         top: calc(var(--cartComponent-mobile-height) + 4.5rem);
         left: 0;
         right: 0;
         margin: 10px auto;
-        .container{
+
+        .container {
             display: grid;
             grid-auto-rows: auto;
             grid-template-columns: 1fr;
             gap: 5px;
         }
-        .dish{
+
+        .dish {
             padding: 0 5px;
             position: relative;
             max-height: 150px;
@@ -165,36 +176,47 @@ a{
             align-items: center;
             font-size: 1.2rem;
             margin-bottom: 10px;
-            .name{
+
+            .name {
                 font-size: 1.2rem;
                 line-height: 1.5rem;
                 max-height: 3rem;
                 overflow: hidden;
             }
-            img{
+
+            img {
                 margin: auto 5px;
                 width: 80px;
                 height: 80px;
+
                 @media screen and (min-width: 968px) {
                     width: 100px;
                     height: 100px;
                 }
+
                 object-fit: contain;
                 object-position: center;
             }
-            .quantity-buttons{
+
+            .quantity-buttons {
                 background-color: var(--bg-primary-color);
                 align-items: center;
                 border-radius: 10px;
                 max-height: 50px;
             }
-            .description{
+
+            .btn-hover:hover {
+                color: white;
+            }
+
+            .description {
                 font-size: .8rem;
                 line-height: 1rem;
                 overflow: hidden;
                 max-height: 2rem;
             }
-            &::after{
+
+            &::after {
                 content: '';
                 position: absolute;
                 bottom: -10px;
@@ -205,11 +227,13 @@ a{
             }
         }
     }
-    .bottom-cart{
+
+    .bottom-cart {
         bottom: 0;
         max-height: 5rem;
     }
-    .button{
+
+    .button {
         padding: 20px;
         white-space: nowrap;
         margin: 1rem;
@@ -221,22 +245,40 @@ a{
         justify-content: center;
         align-items: center;
         color: white;
-        a{
+
+        a {
             color: white;
 
-            span{
+            span {
                 color: white;
             }
         }
-        &.red{
+
+        &.red {
             width: 25%;
             background-color: red;
         }
     }
+}</style>
 
-    .empty-chart {
-        color: var(--bg-secondary-color);
-    }
-}
 
-</style>
+
+
+
+<!-- se il carrello è pieno -->
+<!-- <div v-if="cart.dishes.length" class=""> -->
+                        
+<!-- se il carrello è vuoto -->
+<!-- <div v-else>
+    <h2 class="empty-chart">
+        Il carrello è vuoto!
+    </h2>
+</div> -->
+           
+
+
+
+
+<!-- .empty-chart {
+    color: var(--bg-secondary-color);
+} -->
